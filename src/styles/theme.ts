7e30175cd2ab011806ab764cc4996a0223fd4b6a
LoLoSenPai <loic.dlugosz@gmail.com>
2024-02-07 17:13:26 +0100

    first commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const color = {
+    primary: "#F0E4CF",
+    secondary: "#0E1921",
+    secondaryLight: "#0d1b2a",
+    black: "#0A0A0B",
+    white: "#F8F7FF",
+    whiteShade: "#A8ABB4",
+    bg:"#1b263b",
+}
